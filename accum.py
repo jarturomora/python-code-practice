a69f7d0894a2248c0a6e19dca8b74ca7aa1d6fd9
jarturomora <jarturomora@gmail.com>
2016-08-14 18:19:10 -0500

    first commit with my first exersice

---
+++ accum.py
@@ -0,0 +1,40 @@
+################################################################################
+# This time no story, no theory.
+# The examples below show you how to write function accum:
+
+# Examples:
+
+# accum("abcd")    # "A-Bb-Ccc-Dddd"
+# accum("RqaEzty") # "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+# accum("cwAt")    # "C-Ww-Aaa-Tttt"
+
+# The parameter of accum is a string which includes only letters
+# from a..z and A..Z.
+################################################################################
+
+def accum(s):
+    lim = len(s) + 1
+    result = ''
+    for i in xrange(1, lim):
+        result = result + (s[i-1] * i).title() + '-'
+    result = result[:len(result) - 1]
+    return result
+
+# Simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+
+def main():
+    test(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
+    test(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
+    test(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")
+    test(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")
+    test(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")
+
+if __name__ == '__main__':
+    main()
